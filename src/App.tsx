5c1956733da095fc257e1b0a089916e82a997a0f
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2023-03-16 15:56:23 +0100

    [README] Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './tools/Header';
-import Home from './pages/Home';
-import About from './pages/About';
-import Footer from './tools/Footer';
-import Service from './pages/Service';
-import Feature from './pages/Feature';
-import Appointment from './pages/Appointment';
-import Team from './pages/Team';
-import Testimonial from './pages/Testimonial';
-import NotFound from './pages/NotFound';
-import Contact from './pages/Contact';
-
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./tools/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Footer from "./tools/Footer";
+import Service from "./pages/Service";
+import Feature from "./pages/Feature";
+import Appointment from "./pages/Appointment";
+import Team from "./pages/Team";
+import Testimonial from "./pages/Testimonial";
+import NotFound from "./pages/NotFound";
+import Contact from "./pages/Contact";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <>
-<Header/>
-<Routes >
-  <Route path='/' element={<Home/>}/>
-  <Route path='/about' element={<About/>}/>
-  <Route path='/service' element={<Service/>}/>
-  <Route path='/feature' element={<Feature/>}/>
-  <Route path='/appointment' element={<Appointment/>}/>
-  <Route path='/team' element={<Team/>}/>
-  <Route path='/testimonial' element={<Testimonial/>}/>
-  <Route path='/404' element={<NotFound/>}/>
-  <Route path='/contact' element={<Contact/>}/>
-  <Route path='*' element={<NotFound/>}/>
-</Routes>
-<Footer/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/feature" element={<Feature />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/testimonial" element={<Testimonial />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
--- src/tools/Header.tsx
+++ src/tools/Header.tsx
@@ -1,54 +1,58 @@
- import { faFacebookF, faInstagram, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faClock, faEnvelopeOpen } from '@fortawesome/free-regular-svg-icons';
-import { faArrowUp, faBars, faPhone } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebookF,
+  faInstagram,
+  faLinkedinIn,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faClock, faEnvelopeOpen } from "@fortawesome/free-regular-svg-icons";
+import { faArrowUp, faBars, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from 'react';
-import { Link, NavLink, useLocation } from 'react-router-dom';
-import styled from 'styled-components';
-import { Tablet } from './responsive';
-
+import React from "react";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import { Tablet } from "./responsive";
+const Container = styled.div``;
 const Top = styled.div`
-display: inline-flex;
-padding: 8px 48px;
-align-items: center;
-justify-content: space-between;
+  display: inline-flex;
+  padding: 8px 48px;
+  align-items: center;
+  justify-content: space-between;
   height: 40px;
   width: 100%;
-  background-color: #15233C;
-  top:0;
-  ${Tablet({display:"none"})}
+  background-color: #15233c;
+  top: 0;
+  ${Tablet({ display: "none" })}
 `;
 const TopLeft = styled.div`
- height: 100%;
+  height: 100%;
   display: flex;
   gap: 28px;
 `;
 const TopLeftRow = styled.div`
   display: inline-flex;
-  align-items:center;
+  align-items: center;
   color: rgba(255, 255, 255, 0.502);
   gap: 8px;
 `;
 const TopLeftRowText = styled.small`
   font-size: 0.875em;
-  color:rgba(255,255,255,0.5);
+  color: rgba(255, 255, 255, 0.5);
 `;
 
 const TopRight = styled.div`
- height: 100%;
+  height: 100%;
   display: flex;
   align-items: center;
   gap: 24px;
 `;
 const TopIcon = styled(FontAwesomeIcon)`
-  cursor:pointer;
+  cursor: pointer;
   & path {
     color: rgba(255, 255, 255, 0.502);
-
   }
-`
+`;
 interface BottomProps {
-  isSticky:boolean
+  isSticky: boolean;
 }
 const Bottom = styled.div<BottomProps>`
   padding: 8px 48px;
@@ -57,181 +61,180 @@ const Bottom = styled.div<BottomProps>`
   align-items: center;
   width: 100%;
   transition: all 500ms ease;
-  position:sticky;
-  top:${({ isSticky }) => isSticky ? 0 : -100}px;
-box-shadow: ${({ isSticky }) => isSticky ?  "0 0.125rem 0.25rem rgba(0,0,0,0.08)" : "" };;
-  z-index:1;
-  background: #FFF;
+  position: sticky;
+  top: ${({ isSticky }) => (isSticky ? 0 : -100)}px;
+  box-shadow: ${({ isSticky }) =>
+    isSticky ? "0 0.125rem 0.25rem rgba(0,0,0,0.08)" : ""};
+  z-index: 1;
+  background: #fff;
   ${Tablet({
     paddingLeft: "24px",
     paddingRight: "24px",
-    flexWrap:"wrap",
+    flexWrap: "wrap",
   })}
-`; 
+`;
 export const LogoContainer = styled(Link)`
-display: inline-flex;
-align-items: center;
-gap: 16px;
-cursor: pointer;
-text-decoration: none;
+  display: inline-flex;
+  align-items: center;
+  gap: 16px;
+  cursor: pointer;
+  text-decoration: none;
 `;
 export const Title = styled.h1`
-  ${
-    Tablet({
-      fontSize: "calc(1.375rem + 1.5vw)"
-    })
-  }
+  ${Tablet({
+    fontSize: "calc(1.375rem + 1.5vw)",
+  })}
 `;
 export const Logo = styled.img`
   height: 60px;
-  ${Tablet({height:45})}
+  ${Tablet({ height: 45 })}
 `;
 interface NavProps {
-  expanded: boolean
+  expanded: boolean;
 }
 const NavContainer = styled.div<NavProps>`
-${({ expanded }) =>Tablet({
-  flexBasis: "100%",
-  flexGrow: 1,
-  transition: "max-height 350ms ease",
-  maxHeight:  expanded ? 500 : 0 ,
-  overflow: "hidden",
-})}
-  `;
+  ${({ expanded }) =>
+    Tablet({
+      flexBasis: "100%",
+      flexGrow: 1,
+      transition: "max-height 350ms ease",
+      maxHeight: expanded ? 500 : 0,
+      overflow: "hidden",
+    })}
+`;
 const Nav = styled.nav`
   display: flex;
   gap: 24px;
-  background-color: #F6F7FC;
+  background-color: #f6f7fc;
   border-radius: 10px;
   padding: 8px 16px;
   ${Tablet({
     flexDirection: "column",
-    gap:0,
-    marginTop:24,
-    paddingTop:24,
-    paddingBottom:24,
+    gap: 0,
+    marginTop: 24,
+    paddingTop: 24,
+    paddingBottom: 24,
   })}
 `;
 interface NavItemProps {
-  color: string
+  color: string;
 }
 const NavItem = styled(NavLink)<NavItemProps>`
-  color:#${({ color }) => color };
+  color: #${({ color }) => color};
   transition: 350ms color ease-in;
   cursor: pointer;
   text-decoration: none;
   &:hover {
-    color:#275FC9;
+    color: #275fc9;
     & path {
-      color:#275FC9;
+      color: #275fc9;
     }
   }
   font-weight: 600;
   font-size: 16px;
-  ${Tablet({padding: "8px 0"})}
+  ${Tablet({ padding: "8px 0" })}
 `;
 const DropDown = styled.div`
-  position:relative;
+  position: relative;
   &:hover {
-    & > div{
+    & > div {
       visibility: visible;
-      opacity:1;
+      opacity: 1;
       transform: translateY(-32px);
       // Reset to initial state
-  ${Tablet({
-      transform: "translateY(0)",
-  })} 
+      ${Tablet({
+        transform: "translateY(0)",
+      })}
     }
   }
   ${Tablet({
     padding: "8px 0",
-  })} 
+  })}
 `;
 interface DropDownNavItemProps {
-  selected:boolean
+  selected: boolean;
 }
 const DropDownNavItem = styled(NavItem)<DropDownNavItemProps>`
-  ::after{
+  ::after {
     content: "\f107";
     font-family: "Font Awesome 5 Free";
     font-weight: 900;
-    display:none;
+    display: none;
   }
   & svg {
-    margin-left:8px;
+    margin-left: 8px;
   }
-  color: #${({ selected }) => selected ? "015FC9" :" 696E77" };
+  color: #${({ selected }) => (selected ? "015FC9" : " 696E77")};
   & path {
-    color: #${({ selected }) => selected ? "015FC9" :" 696E77" };
+    color: #${({ selected }) => (selected ? "015FC9" : " 696E77")};
   }
 `;
 interface DropDownListProps {
-  expand:boolean
+  expand: boolean;
 }
 const DropDownList = styled.div<DropDownListProps>`
-  position:absolute;
+  position: absolute;
   padding: 8px 0px;
-  display:flex;
-  flex-direction:column;
-  background-color: #F4F6FB;
+  display: flex;
+  flex-direction: column;
+  background-color: #f4f6fb;
   border-radius: 10px;
-  right:0;
-  top:64px;
+  right: 0;
+  top: 64px;
   visibility: hidden;
-  opacity:0;
+  opacity: 0;
   transform: translateY(0);
   transition: all 500ms ease 0s;
-  ${
-   ({expand})=> Tablet({
-      position:"static",
-      visibility:"visible",
+  ${({ expand }) =>
+    Tablet({
+      position: "static",
+      visibility: "visible",
       opacity: 1,
-      display: expand ? "flex" : "none"
-    })
-  }
-  `;
-  interface DropDownItemProps {
-    selected:boolean
-  }
-const DropDownItem = styled(NavLink)<DropDownItemProps>`
-width: 160px;
-height:32px;
-padding:4px 16px;
-box-sizing: border-box;
-text-decoration: none;
-color: #${({ selected }) => selected ? "FFF" : "1C1E21"};
-&:hover {
-  background-color: #E5E9EC;
+      display: expand ? "flex" : "none",
+    })}
+`;
+interface DropDownItemProps {
+  selected: boolean;
 }
-background-color: ${({ selected }) => selected ? "#015FC9" :"transparent" };
+const DropDownItem = styled(NavLink)<DropDownItemProps>`
+  width: 160px;
+  height: 32px;
+  padding: 4px 16px;
+  box-sizing: border-box;
+  text-decoration: none;
+  color: #${({ selected }) => (selected ? "FFF" : "1C1E21")};
+  &:hover {
+    background-color: #e5e9ec;
+  }
+  background-color: ${({ selected }) => (selected ? "#015FC9" : "transparent")};
 `;
 const QuoteButton = styled.a`
   border-radius: 10px;
-  color: #DFEBF8;
-  background-color: #275FC9;
+  color: #dfebf8;
+  background-color: #275fc9;
   padding: 6px 16px;
   line-height: 1.5;
   font-weight: 400;
   border: 1px solid transparent;
-  cursor:pointer;
+  cursor: pointer;
   user-select: none;
-  font-size:16px;
+  font-size: 16px;
   transition: all 0.5s ease;
   &:hover {
-    background: #00D1EC;
+    background: #00d1ec;
   }
   ${Tablet({
-    display: "none"
+    display: "none",
   })}
 `;
 
 const Bars = styled.button`
- display:none;
-${Tablet({display:"initial"})}
+  display: none;
+  ${Tablet({ display: "initial" })}
   border: 1px solid #E5E5E5;
   background-color: transparent;
   color: #737373;
-  cursor:pointer;
+  cursor: pointer;
   padding: 8px 16px;
   border-radius: 10px;
   transition: all 150ms ease-in-out;
@@ -240,141 +243,182 @@ ${Tablet({display:"initial"})}
   }
 `;
 interface ScrollToTopBtnProps {
-  visible:boolean
+  visible: boolean;
 }
 const ScrollToTopBtn = styled.button<ScrollToTopBtnProps>`
-  height:48px;
-  width:48px;
-  z-index:1;
-  position:fixed;
-  bottom:30px;
-  right:30px;
-  background-color: #015FC9;
-  border:1px solid #015FC9;
+  height: 48px;
+  width: 48px;
+  z-index: 1;
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+  background-color: #015fc9;
+  border: 1px solid #015fc9;
   border-radius: 10px;
   transition: all 150ms ease-out, opacity 500ms ease-in;
-  cursor:pointer;
-  opacity:${({ visible }) => visible ? 1 : 0};
-  visibility:${({ visible }) => visible ? "visible" : "hidden"};
+  cursor: pointer;
+  opacity: ${({ visible }) => (visible ? 1 : 0)};
+  visibility: ${({ visible }) => (visible ? "visible" : "hidden")};
   &:hover {
-    border-color:#0DD3F1;
-    background-color: #0DD3F1;
+    border-color: #0dd3f1;
+    background-color: #0dd3f1;
   }
   &:focus {
-   box-shadow: 0 0 0 0.25rem rgba(39,119,209,0.5);
+    box-shadow: 0 0 0 0.25rem rgba(39, 119, 209, 0.5);
   }
 `;
 const BtnIcon = styled(FontAwesomeIcon)`
   & path {
-    color:#FFF;
+    color: #fff;
   }
-  
 `;
 const Header = () => {
   const [activeLink, setActiveLink] = React.useState("home");
-   const {pathname} = useLocation()
-   const [isDesktop, setIsDesktop] = React.useState(false);
-    React.useEffect(() => {
-  const handleWindowSizeChange = () => {   
-    if(window.innerWidth >= 992) // 992 is the Tablet breakpoint on the CSS
-    setIsDesktop(true)
-    else
-    setIsDesktop(false)
-   }
-   window.addEventListener("resize",handleWindowSizeChange)
-   return ()=>{
-     window.removeEventListener("resize",handleWindowSizeChange)
-    }
-  }, [])
+  const { pathname } = useLocation();
+  const [isDesktop, setIsDesktop] = React.useState(false);
+  React.useEffect(() => {
+    const handleWindowSizeChange = () => {
+      if (window.innerWidth >= 992)
+        // 992 is the Tablet breakpoint on the CSS
+        setIsDesktop(true);
+      else setIsDesktop(false);
+    };
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
 
-   React.useEffect(() => {
-    if(["/feature",
-    "/appointment",
-    "/team",
-    "/testimonial",
-    "/404"].includes(pathname))
-    setActiveLink("page")
-    else
-    setActiveLink(pathname)
-   }, [pathname])
-   const [showNavbar, setShowNavbar] = React.useState(false);
-   const [showDropDownList, setShowDropDownList] = React.useState(false);
-   
-   const [isSticky, setIsSticky] = React.useState(false);
-   React.useEffect(()=>{
-    const handleScroll = ()=> {
-      if(window.scrollY > 300)
-      setIsSticky(true)
-      else
-      setIsSticky(false)
-    }
-    window.addEventListener("scroll", handleScroll)
-    return ()=> window.removeEventListener("scroll", handleScroll)
-   })
-   const handleColor = (link:string) => activeLink === link ? "275FC9": "696E77"
-   const handleScrollToTop = () => {
-   window.scroll({
-    top:0,
-    behavior:"smooth"
-   })}
+  React.useEffect(() => {
+    if (
+      ["/feature", "/appointment", "/team", "/testimonial", "/404"].includes(
+        pathname
+      )
+    )
+      setActiveLink("page");
+    else setActiveLink(pathname);
+  }, [pathname]);
+  const [showNavbar, setShowNavbar] = React.useState(false);
+  const [showDropDownList, setShowDropDownList] = React.useState(false);
+
+  const [isSticky, setIsSticky] = React.useState(false);
+  React.useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) setIsSticky(true);
+      else setIsSticky(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+  const handleColor = (link: string) =>
+    activeLink === link ? "275FC9" : "696E77";
+  const handleScrollToTop = () => {
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   return (
     <>
-      <Top>
-        <TopLeft>
-          <TopLeftRow>
-            <TopIcon icon={faPhone} size="sm"  />
-            <TopLeftRowText>+012 345 6789</TopLeftRowText>
-          </TopLeftRow>
-          <TopLeftRow>
-            <TopIcon icon={faEnvelopeOpen} size="sm" />
-            <TopLeftRowText>info@example.com</TopLeftRowText>
-          </TopLeftRow>
-          <TopLeftRow>
-            <TopIcon icon={faClock} size="sm"/>
-            <TopLeftRowText>Mon - Fri : 09 AM - 09 PM
-            </TopLeftRowText>
-          </TopLeftRow>
-        </TopLeft>
-        <TopRight>
-          <TopIcon icon={faFacebookF}/>
-          <TopIcon icon={faTwitter}/>
-          <TopIcon icon={faLinkedinIn}/>
-          <TopIcon icon={faInstagram}/>
-        </TopRight>
-      </Top>
-      <Bottom isSticky={isSticky}>
-        <LogoContainer to="/">
-      <Logo src={`${process.env.PUBLIC_URL}/assets/icon/icon-02-primary.png`}/>
-        <Title>Insure</Title>
-        </LogoContainer>
-        <Bars onClick={()=>setShowNavbar(!showNavbar)}>
-          <FontAwesomeIcon icon={faBars} size="2x" />
-        </Bars>
-        <NavContainer expanded={showNavbar}>
-        <Nav>
-          <NavItem to="/" color={handleColor("/")} >Home</NavItem>
-          <NavItem to="/about" color={handleColor("/about")} >About Us</NavItem>
-          <NavItem to="/service" color={handleColor("/service")} >Our Services</NavItem>
-          <DropDown onClick={()=> !isDesktop && setShowDropDownList(!showDropDownList) }>
-          <DropDownNavItem to="" onClick={(e)=>e.preventDefault()} color={handleColor("/page")} selected={activeLink === "page"}>Pages</DropDownNavItem>
-          <DropDownList expand={showDropDownList}>
-            <DropDownItem selected={pathname === "/feature"} to="/feature">Features</DropDownItem>
-            <DropDownItem selected={pathname === "/appointment"} to="/appointment">Appointment</DropDownItem>
-            <DropDownItem selected={pathname === "/team"} to="/team">Team Members</DropDownItem>
-            <DropDownItem selected={pathname === "/testimonial"} to="/testimonial">Testimonial</DropDownItem>
-            <DropDownItem selected={pathname === "/404"} to="/404">404 Page</DropDownItem>
-          </DropDownList>
-          </DropDown>
-          <NavItem to="contact" color={handleColor("/contact")}>Contact Us</NavItem>
-        </Nav>
-        </NavContainer>
-        <QuoteButton>Get A Quote</QuoteButton>
-      </Bottom>
-      {<ScrollToTopBtn visible={isSticky} onClick={handleScrollToTop}>
-        <BtnIcon icon={faArrowUp} size="lg"/>
-        </ScrollToTopBtn>}
+      <Container data-aos="fade-in" data-aos-delay="500">
+        <Top>
+          <TopLeft>
+            <TopLeftRow>
+              <TopIcon icon={faPhone} size="sm" />
+              <TopLeftRowText>+012 345 6789</TopLeftRowText>
+            </TopLeftRow>
+            <TopLeftRow>
+              <TopIcon icon={faEnvelopeOpen} size="sm" />
+              <TopLeftRowText>info@example.com</TopLeftRowText>
+            </TopLeftRow>
+            <TopLeftRow>
+              <TopIcon icon={faClock} size="sm" />
+              <TopLeftRowText>Mon - Fri : 09 AM - 09 PM</TopLeftRowText>
+            </TopLeftRow>
+          </TopLeft>
+          <TopRight>
+            <TopIcon icon={faFacebookF} />
+            <TopIcon icon={faTwitter} />
+            <TopIcon icon={faLinkedinIn} />
+            <TopIcon icon={faInstagram} />
+          </TopRight>
+        </Top>
+        <Bottom isSticky={isSticky}>
+          <LogoContainer to="/">
+            <Logo
+              src={`${process.env.PUBLIC_URL}/assets/icon/icon-02-primary.png`}
+            />
+            <Title>Insure</Title>
+          </LogoContainer>
+          <Bars onClick={() => setShowNavbar(!showNavbar)}>
+            <FontAwesomeIcon icon={faBars} size="2x" />
+          </Bars>
+          <NavContainer expanded={showNavbar}>
+            <Nav>
+              <NavItem to="/" color={handleColor("/")}>
+                Home
+              </NavItem>
+              <NavItem to="/about" color={handleColor("/about")}>
+                About Us
+              </NavItem>
+              <NavItem to="/service" color={handleColor("/service")}>
+                Our Services
+              </NavItem>
+              <DropDown
+                onClick={() =>
+                  !isDesktop && setShowDropDownList(!showDropDownList)
+                }
+              >
+                <DropDownNavItem
+                  to=""
+                  onClick={(e) => e.preventDefault()}
+                  color={handleColor("/page")}
+                  selected={activeLink === "page"}
+                >
+                  Pages
+                </DropDownNavItem>
+                <DropDownList expand={showDropDownList}>
+                  <DropDownItem
+                    selected={pathname === "/feature"}
+                    to="/feature"
+                  >
+                    Features
+                  </DropDownItem>
+                  <DropDownItem
+                    selected={pathname === "/appointment"}
+                    to="/appointment"
+                  >
+                    Appointment
+                  </DropDownItem>
+                  <DropDownItem selected={pathname === "/team"} to="/team">
+                    Team Members
+                  </DropDownItem>
+                  <DropDownItem
+                    selected={pathname === "/testimonial"}
+                    to="/testimonial"
+                  >
+                    Testimonial
+                  </DropDownItem>
+                  <DropDownItem selected={pathname === "/404"} to="/404">
+                    404 Page
+                  </DropDownItem>
+                </DropDownList>
+              </DropDown>
+              <NavItem to="contact" color={handleColor("/contact")}>
+                Contact Us
+              </NavItem>
+            </Nav>
+          </NavContainer>
+          <QuoteButton>Get A Quote</QuoteButton>
+        </Bottom>
+      </Container>
+      {
+        <ScrollToTopBtn visible={isSticky} onClick={handleScrollToTop}>
+          <BtnIcon icon={faArrowUp} size="lg" />
+        </ScrollToTopBtn>
+      }
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
